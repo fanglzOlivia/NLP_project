0dd6246f80a2f3eb6d5129fd9a8185c63afb2faa
jtfoo2 <jtfoo2@gmail.com>
2018-04-06 01:10:30 -0400

    Add files via upload

--- parsingXML.py
+++ parsingXML.py
@@ -1,22 +1,66 @@
 import xml.etree.ElementTree as ET
-filename = 'AnnotationFinal0-80/goldstandard0.xml'
-fileid = filename.replace('AnnotationFinal0-80/goldstandard', '').replace('.xml', '')
+import glob
+
+#filename = 'AnnotationFinal0-80/goldstandard0.xml'
+gsFiles = sorted(glob.glob("/Users/julia/Desktop/NLP/AnnotationFinal0-80/*.xml"))
+
+fileid = filename.replace('/Users/julia/Desktop/NLP/AnnotationFinal0-80/goldstandard', '').replace('.xml', '')
 
 tree = ET.parse(filename)
 root = tree.getroot()
 
 timexCols = ['fileId', 'timexId', 'spanStart', 'spanEnd', 'value']
 timexLst = []
+eventCols = ['fileId', 'eventId', 'spanStart', 'span1', 'span2', 'spanEnd', 'text', 'class', 'eventType', 'polarity', 'completion']
+eventLst = []
+tLinkCols = ['fileId', 'tlinkId', 'timexId', 'eventId']
+tLinkLst = []
 
-# pull out attributes for each TIMEX3 element in the file and add to list
-for ann in root.iter('TIMEX3'):
-    #print('ID', ann.attrib['id'])
-    tid = ann.attrib['id']
-    [spanStart, spanEnd] = [int(r) for r in ann.attrib['spans'].split('~')]
-    val = ann.find('value')
-    if val is not None:
-        val = ann.attrib['value']
-    else:
-        val = ann.attrib['text']
+# pull out attributes for each type of annotation element in the file and add to list
+for filename in gsFiles:
+    fileid = filename.replace('/Users/julia/Desktop/NLP/AnnotationFinal0-80/goldstandard', '').replace('.xml', '')
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    #print(fileid)
+    
+    for ann in root.iter('TIMEX3'):
+        #print('ID', ann.attrib['id'])
+        tid = ann.attrib['id']
+        [spanStart, spanEnd] = [int(r) for r in ann.attrib['spans'].split('~')]
+        fx = ann.attrib['functionInDocument']
+        if 'value' in ann.attrib:
+            val = ann.attrib['value']
+        else:
+            val = ann.attrib['text']
 
-    timexLst.append([fileid, tid, spanStart, spanEnd, val])
+        timexLst.append([fileid, tid, spanStart, spanEnd, val, fx])
+    
+    for ann in root.iter('EVENT'):
+        eid = ann.attrib['id']
+        sp = ann.attrib['spans']
+        if sp.find(',') > 0:
+            [spanStart, span1, span2, spanEnd] = [int(r) for r in re.split('[~,]', sp)]
+        else:
+            [spanStart, spanEnd] = [int(r) for r in sp.split('~')]
+            [span2, span1] = [int(r) for r in sp.split('~')]
+        text = ann.attrib['text']
+        cls = ann.attrib['class']
+        et = ann.attrib['eventType']
+        pol = ann.attrib['polarity']
+        if 'completion' in ann.attrib:
+            comp = ann.attrib['completion']
+        else: comp = None
+        
+        eventLst.append([fileid, eid, spanStart, span1, span2, spanEnd, text, cls, et, pol, comp])
+    
+    for ann in root.iter('TLINK'):
+        tlid = ann.attrib['id']
+        tid = ann.attrib['fromID']
+        eid = ann.attrib['toID']
+        
+        tLinkLst.append([fileid, tlid, tid, eid])
+        
+
+dfTimex = pd.DataFrame(timexLst, columns=timexCols)
+dfEvent = pd.DataFrame(eventLst, columns=eventCols)
+dfTLink = pd.DataFrame(tLinkLst, columns=tLinkCols)
